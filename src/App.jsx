4724774e205c3ced68b97e7e0d2e64502f3b62a9
Jillian Bleggi <jillian.bleggi@gmail.com>
2025-03-11 15:34:03 -0400

    Added Header to Main

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import Logo from '../public/favicon.ico'
+
 import './App.css'
 
 function App() {
--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,8 +1,42 @@
+import  { useState, useEffect } from 'react';
+import Logo from '../public/favicon.ico';
+
+function fetchMain(){
+  const [subscriptions, setSubscriptions] = useState({});
+  const [error, setError] = useState("");
 
 
-function getSubscriptions() {
-  fetch('http://localhost:3001/api/v1/subscriptions')
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.log(error.message))
+  function getSubscriptions() {
+    fetch('http://localhost:3001/api/v1/subscriptions')
+    .then(response => response.json())
+    .then((data) => {
+      setSubscriptions(data.subscriptions);
+    })
+    .catch(error => console.log(error.message))
+  };
+
+  useEffect(() => {
+    getSubscriptions();
+  }, []);
+
+  return (
+    <main className="mainPage">
+      <nav className="main-header">
+        <img src={Logo} alt="Tea Time Logo" className="App-logo"></img>
+        <h1>Title: Tea Time</h1>
+        <h2>Welcome, Shop Owner!</h2>
+        <p>Select a subscription below to get started.</p>
+      </nav>
+
+      <nav className="filter-component">
+
+      </nav>
+
+      <nav className="subscription-container">
+        
+      </nav>
+
+    </main>
+  )
+
 }
